2c233fe6c15482a97a61380278fecf7a76248953
jherrera10 <juan.herrera.barrios@gmail.com>
2019-03-11 13:16:47 -0500

    Contact Us

---
+++ ContactUs.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Automation01
+{
+    [TestClass]
+    public class ContactUs
+    {
+        IWebDriver driver;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver.Url = "http://www.automationpractice.com";
+            driver.FindElement(By.Id("contact-link")).Click();
+            new SelectElement(driver.FindElement(By.Id("id_contact"))).SelectByValue("2");
+            driver.FindElement(By.Id("email")).SendKeys("Test@mailinator.com");
+            driver.FindElement(By.Id("id_order")).SendKeys("20204");
+            driver.FindElement(By.Id("message")).SendKeys("This is a test");
+            driver.FindElement(By.Id("submitMessage")).Click();
+            string sucess = driver.FindElement(By.XPath("//*[@id=\"center_column\"]/ p")).Text;
+
+            Assert.AreEqual("Your message has been successfully sent to our team.", sucess);
+
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            driver.Quit();
+        }
+
+
+    }
+
+
+
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("70af9afa-2ec6-425d-8fd7-1bf63b03c7e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
